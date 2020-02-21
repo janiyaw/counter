f84baab6df389e44fe185c6d1195ff4e6043ad50
MMCR <janiya.whitt@yahoo.com>
2020-02-20 20:14:38 -0800

    Added the increment and decrement methods aye we lit :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component {
+constructor () {
+  super ();
+  this.state = {
+    /*state is something
+  that's gonna change  */
+    count: 0,
+  }
 }
 
-export default App;
+//this is where our methods go
+increment = () => {
+  this.setState({
+    count: this.state.count + 1,
+  })
+}
+
+decrement = () => {
+  this.setState ({
+    count: this.state.count - 1,
+  })
+}
+
+  render () {
+    return (
+      <div className = "container">
+      <div className = "navbar">Counter</div>
+      <div className = "counter">
+      <h1>{this.state.count}</h1>
+      <button type="button">Clear</button>
+      <button type="button" onClick={this.increment}>Increment</button>
+      <button type="button" onClick={this.decrement}>Decrement</button>
+      <button type="button">Single Count</button>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default Counter;
